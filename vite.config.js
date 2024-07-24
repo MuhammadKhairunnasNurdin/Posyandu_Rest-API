e76f771eac77428c773de45fa38e303d34ce7728
MuhammadKhairunnasNurdin <anasnurdin936@gmail.com>
2024-07-24 23:17:52 +0700

    Feat(Setup Dependency): add laravel sail for docker

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import laravel from 'laravel-vite-plugin';
+
+const env = loadEnv(process.env.APP_ENV, process.cwd());
 
 export default defineConfig({
     plugins: [
@@ -8,4 +10,17 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true
+        },
+        /**
+         * configure for multiple running vite port
+         * so we can run multiple laravel project with vite
+         * */
+        port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 5173,
+    },
 });
